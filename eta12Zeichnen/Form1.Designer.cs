9498c2a2b0fcd5c2b9b51b96e011e6374957e973
Peer Niewolick <Peer.Mathia@web.de>
2012-06-20 13:53:20 +0200

    first reales kreis only
    nur kreis zeichen

---
+++ eta12Zeichnen/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelZumReinzeichnen = new System.Windows.Forms.Panel();
+            this.buttonZumKreisMalen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelZumReinzeichnen
+            // 
+            this.panelZumReinzeichnen.Location = new System.Drawing.Point(12, 107);
+            this.panelZumReinzeichnen.Name = "panelZumReinzeichnen";
+            this.panelZumReinzeichnen.Size = new System.Drawing.Size(463, 298);
+            this.panelZumReinzeichnen.TabIndex = 0;
+            this.panelZumReinzeichnen.Paint += new System.Windows.Forms.PaintEventHandler(this.panelZumReinzeichnen_Paint);
+            // 
+            // buttonZumKreisMalen
+            // 
+            this.buttonZumKreisMalen.Location = new System.Drawing.Point(12, 12);
+            this.buttonZumKreisMalen.Name = "buttonZumKreisMalen";
+            this.buttonZumKreisMalen.Size = new System.Drawing.Size(75, 23);
+            this.buttonZumKreisMalen.TabIndex = 1;
+            this.buttonZumKreisMalen.Text = "Kreis Zeichen";
+            this.buttonZumKreisMalen.UseVisualStyleBackColor = true;
+            this.buttonZumKreisMalen.Click += new System.EventHandler(this.buttonZumKreisMalen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 417);
+            this.Controls.Add(this.buttonZumKreisMalen);
+            this.Controls.Add(this.panelZumReinzeichnen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelZumReinzeichnen;
+        private System.Windows.Forms.Button buttonZumKreisMalen;
+    }
+}
+
---
+++ eta12Zeichnen/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool sollKreisGezeichnetWerden = false;//eine variable um zu prüfen ob ein kreis gezeichnet werden soll
+        private void buttonZumKreisMalen_Click(object sender, EventArgs e)//das ereignis das ausgelöst wird wenn der button gedrückt wird. sender wird selten verwendet. 
+                                                                          //mit e kann man sich mehr information holle zu was passiert ist hier selten benutzt 
+        {
+            sollKreisGezeichnetWerden = true; //wenn der button gedrück wurde soll wo anders der kreis gezeichnet werden
+            panelZumReinzeichnen.Refresh();//mit Refresh bringen wir panelzumreinzeichen dazu sich neu zu zeichen und damit ist der kreis gleich sichtbar
+        }
+        //in EnventArgs und auch PaintEventArgs sind informationen zu dem eregniss. uns fehlt das wissen um sie allgemein zu nutzten, also werd ich es explizit erwähen wie wir sie in ein paar fällen butzten können
+
+        /// <summary>
+        /// panelZumReinzeichnen_Paint: das ereignis welches ausgelöst wird wenn das pannel neu gezeichet wird
+        /// </summary>
+        /// <param name="sender">haben wir noch nicht benutzt</param>
+        /// <param name="e">aus dem kann man das Graphics objekt hollen </param>
+        private void panelZumReinzeichnen_Paint(object sender, PaintEventArgs e)//das ereigniss 
+        {
+            Graphics grafikDieWirJetztBenutzten = e.Graphics; // das grafik objekt aus dem PaintEventArgs e hollen um leichter dran zu kommen; optional
+            //region ist nur für übersichtlichkeit da. braucht ihr nicht wissen
+            #region kreis zeichnen 
+            if (sollKreisGezeichnetWerden) //prüfen ob wir den kreis zeichnen sollen
+            {
+                Pen kreisStift = Pens.Black; // einen stift aus der stifftesammlung Pens hollen; optional
+                int durchmesserKreis = 30; //den durchmeser vom kreis für übersicht in eingene variable;optional
+                int xKoordinateKreis=30;//zur übersicht;oprional
+                int yKoordinateKreis=30;//zur übersicht;optional
+                //es gibt keine methode um einen um direkt einen kreis zu malen also übergeben wir für länge und breite einfach den durchmesserKreis
+                grafikDieWirJetztBenutzten.DrawEllipse(kreisStift, xKoordinateKreis, yKoordinateKreis, durchmesserKreis, durchmesserKreis);
+                //wenn man alle optionalen schritte überspringt braucht man folgeneden code:
+                e.Graphics.DrawEllipse(Pens.Black, 30, 30, 30, 30);
+            }
+
+
+            #endregion
+
+
+        }
+
+    }
+}
